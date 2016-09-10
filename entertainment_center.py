2d2237b0befe522ccff13739edd8d830b43424b7
Lokesh Sanapalli <s.lokesh1729@gmail.com>
2016-09-11 00:17:03 +0530

    Some Changes

---
+++ entertainment_center.py
@@ -0,0 +1,9 @@
+import media
+
+"""sdfosdsf sodnf """
+bahubali = media.Movie("Bahubali","A Revenge Story","https://en.wikipedia.org/wiki/File:Baahubali_poster.jpg","https://www.youtube.com/watch?v=sOEg_YZQsTI")
+print(bahubali.storyline)
+avatar = media.Movie("Avatar","Alien Planet","https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Avatar-Teaser-Poster.jpg/220px-Avatar-Teaser-Poster.jpg","https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+print(avatar.trailer_url)
+#bahubali.show_trailer()
+print(media.Movie.__doc__)
--- send_text.py
+++ send_text.py
@@ -5,8 +5,8 @@ auth_token  = "030dc6fe0fcea0ebd43aff0e34e73b72"  # Your Auth Token from www.twi
 
 client = TwilioRestClient(account_sid, auth_token)
 
-message = client.messages.create(body="Hello from Python",
-    to="+918466905904",    # Replace with your phone number
+message = client.messages.create(body="Orey, money transfer chesesanu... -Lokesh",
+    to="+919966218112",    # Replace with your phone number
     from_="+13158494899") # Replace with your Twilio number
 
 print(message.sid)
